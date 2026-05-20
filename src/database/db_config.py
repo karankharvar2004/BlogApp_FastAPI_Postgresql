c74ab63c5b7cfa45a145e7b29bc5fc1367bb3a93
KaranKharvar7006 <karankharvar2004@gmail.com>
2026-05-20 15:08:11 +0530

    Implemented some major changes just like Minestone Template

--- src/database/db_config.py
+++ src/database/db_config.py
@@ -22,23 +22,39 @@ Base = declarative_base()
 class AsyncDatabaseSession:
 
     def __init__(self):
+
         self._session = None
+
         self._engine = None
 
     def __getattr__(self, name):
-        return getattr(self._session, name)
+
+        return getattr(
+            self._session,
+            name
+        )
 
     def init(self):
-        self._engine = create_async_engine(Config.DATABASE_URL, future=True, echo=True)
-        self._session = sessionmaker(bind=self._engine, class_=AsyncSession, expire_on_commit=False)
 
-    async def get_session(self):
-        async_session = self._session()
+        self._engine = create_async_engine(
+            Config.DATABASE_URL,
+            future=True,
+            echo=True
+        )
 
-        try:
-            yield async_session
-        finally:
-            await async_session.close()
+        async_session = sessionmaker(
+            bind=self._engine,
+            class_=AsyncSession,
+            expire_on_commit=False
+        )
+
+        self._session = async_session()
+
+    async def close(self):
+
+        if self._session:
+
+            await self._session.close()
 
 
 db = AsyncDatabaseSession()
@@ -47,5 +63,11 @@ db.init()
 
 
 async def get_db():
-    async for session in db.get_session():
-        yield session
+
+    try:
+
+        yield db
+
+    finally:
+
+        pass
